a0fabdfe755f0a26b25f7f633b1b08c06f5a279f
Luska713 <141650325+Luska713@users.noreply.github.com>
2025-04-23 18:11:21 -0300

    Create 01_Python_Intro.py

---
+++ estudos/python/dio/01_Python_Intro.py
@@ -0,0 +1,3 @@
+# Python é uma linguagem de programação criada em 1989 por Guido Van Rossum.
+# O objetivo é que fosse uma linguagem clara e objetiva, de fácil utilização.
+# É uma linguagem multiplataforma de tipagem dinâmica e forte, com uma baixa curva de aprendizado e uma enorme comunidade ativa.
