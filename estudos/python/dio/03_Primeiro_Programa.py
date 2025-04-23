7819733d454d30c5cc54c1dde5f65fdf6c7a4d6d
Luska713 <141650325+Luska713@users.noreply.github.com>
2025-04-23 19:10:15 -0300

    Adicionados arquivos das aulas 1, 2 e 3.

---
+++ estudos/python/dio/03_Primeiro_Programa.py
@@ -0,0 +1,4 @@
+# Os programas em Python geralmente são escritos em arquivos com extensão .py, como este.
+# Segue abaixo um dos programas mais conhecidos de todas as linguagens e programação...
+
+print("Hello World!")
